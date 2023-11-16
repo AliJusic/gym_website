fd4dc9f78fdc1c5128481c7b4907829882242bd2
AliJusic <67058944+AliJusic@users.noreply.github.com>
2023-11-16 16:05:36 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,17 @@
+console.log(bmi);
+
+var bmiButton = document.getElementById("BMIButton");
+const weightInput = document.getElementById("weight");
+const heightInput = document.getElementById("height");
+var text = document.getElementById("bmi");
+
+let height, weight, BMI;
+bmiButton.addEventListener("click", function () {
+  height = heightInput.value / 100;
+  weight = weightInput.value;
+  if (weight > 0 && weight < 400 && height > 0 && height < 400) {
+    BMI = weight / (height * height);
+    BMI = BMI.toFixed(2);
+    text.textContent = `Your BMI is : ${BMI}`;
+  }
+});
